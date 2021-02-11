46a597830f986e975a7f24991b7309dfc60021fb
NikitaBalasahebRaut <68865567+NikitaBalasahebRaut@users.noreply.github.com>
2021-02-11 17:02:36 +0530

    Add files via upload

---
+++ Assignment.php
@@ -0,0 +1,30 @@
+<?php
+
+   $x = 20;
+   $x += 100;
+   echo $x;
+   echo "<br>";
+   
+   $x = 20;
+   $x -= 100;
+   echo $x;
+   echo "<br>";
+   
+   $x = 20;
+   $x *= 100;
+   echo $x;
+   echo "<br>";
+   
+   $x = 20;
+   $x /= 100;
+   echo $x;
+   echo "<br>";
+   
+   $x = 20;
+   $x %= 100;
+   echo $x;
+   echo "<br>";
+   
+   
+   ?>
+   
